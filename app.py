c38e69b8eb38ec48258a4a35da9f6a35892007d3
jax wood <jax.wood.m@gmail.com>
2025-05-29 17:22:36 +0300

    added files

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    my_value = os.getenv('MY_SECRET')
+    return f"The secret value is: {my_value}"
 
 if __name__ == '__main__':
     app.run(debug=True)
